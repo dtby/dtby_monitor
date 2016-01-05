fd5e730bc2891cf8a32ecc2d7c4bda3c6af2b04f
xiaocuixt <cjw624923@126.com>
2016-01-05 16:23:53 +0800

    能效页面

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root "home#index"
+  #告警信息
   resources :alerts, only: :index
+  #能效管理
+  resources :effects, only: :index
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
